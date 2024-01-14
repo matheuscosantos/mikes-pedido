cf30d86393dc51c843ba8b65f25cec70de76e078
GAPrado <guilhermeprado@outlook.com>
2024-01-14 20:20:56 -0300

    add policy

--- infra/main.tf
+++ infra/main.tf
@@ -9,21 +9,28 @@ resource "aws_iam_role" "ecs_execution_role" {
   assume_role_policy = file("iam/role/ecs_execution_role.json")
 }
 
-resource "aws_iam_policy" "ecs_execution_role_additional_policy" {
-  name   = "${var.name}_ecs_execution_role_additional_policy"
-  policy = file("iam/policy/ecs_execution_role_additional_policy.json")
-}
-
-resource "aws_iam_policy_attachment" "ecs_execution_role_additional_policy_attachment" {
-  name       = "${var.name}_ecs_execution_role_additional_policy_attachment"
-  roles      = [aws_iam_role.ecs_execution_role.name]
-  policy_arn = aws_iam_policy.ecs_execution_role_additional_policy.arn
-}
-
 resource "aws_iam_policy_attachment" "ecs_execution_role_ecr_policy_attachment" {
   name       = "${var.name}_ecs_execution_role_ecr_policy_attachment"
   roles      = [aws_iam_role.ecs_execution_role.name]
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+}
+
+resource "aws_iam_policy_attachment" "ecs_execution_role_cloudwatch_policy_attachment" {
+  name       = "${var.name}_ecs_execution_role_cloudwatch_policy_attachment"
+  roles      = [aws_iam_role.ecs_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "ecs_execution_role_sns_policy_attachment" {
+  name       = "${var.name}_ecs_execution_role_sns_policy_attachment"
+  roles      = [aws_iam_role.ecs_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "ecs_execution_role_sqs_policy_attachment" {
+  name       = "${var.name}_ecs_execution_role_sqs_policy_attachment"
+  roles      = [aws_iam_role.ecs_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 }
 
 # -- topics
