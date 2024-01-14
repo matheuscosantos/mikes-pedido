81a12e47fef6f81f91f11b0eb462554404451fef
GAPrado <guilhermeprado@outlook.com>
2024-01-14 12:38:20 -0300

    add log

--- src/main/kotlin/com/mikes/pedido/adapter/inbound/controller/order/OrderController.kt
+++ src/main/kotlin/com/mikes/pedido/adapter/inbound/controller/order/OrderController.kt
@@ -2,15 +2,12 @@ package com.mikes.pedido.adapter.inbound.controller.order
 
 import com.mikes.pedido.adapter.inbound.controller.order.dto.CreateOrderRequest
 import com.mikes.pedido.adapter.inbound.controller.order.dto.OrderDto
-import com.mikes.pedido.adapter.inbound.controller.order.dto.UpdateOrderStatusRequest
 import com.mikes.pedido.application.port.inbound.order.CreateOrderService
 import com.mikes.pedido.application.port.inbound.order.FindOrderService
 import com.mikes.pedido.application.port.inbound.order.UpdateOrderStatusService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -35,17 +32,6 @@ class OrderController(
         @RequestBody createOrderRequest: CreateOrderRequest,
     ): ResponseEntity<OrderDto> {
         return createOrderService.create(createOrderRequest.toInbound())
-            .map { OrderDto.from(it) }
-            .map { ResponseEntity.ok(it) }
-            .getOrThrow()
-    }
-
-    @PutMapping("/{id}/status")
-    fun updateStatus(
-        @PathVariable id: String,
-        @RequestBody updateOrderStatusRequest: UpdateOrderStatusRequest,
-    ): ResponseEntity<OrderDto> { // todo : virar listener?
-        return updateOrderStatusService.update(id, updateOrderStatusRequest.toInbound())
             .map { OrderDto.from(it) }
             .map { ResponseEntity.ok(it) }
             .getOrThrow()
