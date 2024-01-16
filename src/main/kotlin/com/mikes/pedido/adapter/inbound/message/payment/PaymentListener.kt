b1c5724f1c4f17059ec39c44cbd0a18cc95d5df4
GAPrado <guilhermeprado@outlook.com>
2024-01-16 00:13:42 -0300

    criacao das filas

--- src/main/kotlin/com/mikes/pedido/adapter/inbound/message/payment/PaymentListener.kt
+++ src/main/kotlin/com/mikes/pedido/adapter/inbound/message/payment/PaymentListener.kt
@@ -2,8 +2,10 @@ package com.mikes.pedido.adapter.inbound.message.payment
 
 import com.mikes.pedido.adapter.inbound.message.payment.dto.PaymentMessage
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.springframework.stereotype.Component
 import java.util.logging.Logger
 
+@Component
 class PaymentListener {
     @SqsListener("\${sqs.orderPayment.url}")
     fun listener(message: PaymentMessage) {
--- src/main/kotlin/com/mikes/pedido/adapter/inbound/message/production/ProductionListener.kt
+++ src/main/kotlin/com/mikes/pedido/adapter/inbound/message/production/ProductionListener.kt
@@ -2,8 +2,10 @@ package com.mikes.pedido.adapter.inbound.message.production
 
 import com.mikes.pedido.adapter.inbound.message.production.dto.ProductionMessage
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.springframework.stereotype.Component
 import java.util.logging.Logger
 
+@Component
 class ProductionListener {
     @SqsListener("\${sqs.orderProduction.url}")
     fun listener(message: ProductionMessage) {
