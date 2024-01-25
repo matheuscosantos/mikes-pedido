0e46fe9ca9baafc08d05af13acf900c829470375
wagnerlm <wagnerlink@gmail.com>
2024-01-24 22:04:03 -0300

    finish inboud tests

---
+++ src/test/kotlin/com/mikes/pedido/adapter/inbound/controller/exceptionhandler/ControllerExceptionHandlerTest.kt
@@ -0,0 +1,68 @@
+package com.mikes.pedido.adapter.inbound.controller.exceptionhandler
+
+import com.mikes.pedido.application.core.domain.exception.InvalidValueException
+import com.mikes.pedido.application.core.usecase.exception.AlreadyExistsException
+import com.mikes.pedido.application.core.usecase.exception.InvalidDomainStateException
+import com.mikes.pedido.application.core.usecase.exception.NotFoundException
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.context.request.WebRequest
+
+class ControllerExceptionHandlerTest {
+
+    private val webRequest: WebRequest = mockk(relaxed = true)
+    private val controllerExceptionHandler = ControllerExceptionHandler()
+
+    @Test
+    fun testInvalidValueExceptionHandler() {
+        val invalidValueException = InvalidValueException("Invalid value", "fieldName")
+
+        val responseEntity: ResponseEntity<ControllerExceptionHandler.Error> =
+            controllerExceptionHandler.invalidValueExceptionHandler(invalidValueException, webRequest)
+
+        assertErrorResponse(responseEntity, HttpStatus.BAD_REQUEST, ControllerExceptionHandler.TYPE_INVALID_VALUE_EXCEPTION, "Invalid value for 'fieldName': Invalid value")
+    }
+
+    @Test
+    fun testNotFoundExceptionHandler() {
+        val notFoundException = NotFoundException("Entity", "123")
+
+        val responseEntity: ResponseEntity<ControllerExceptionHandler.Error> =
+            controllerExceptionHandler.notFoundExceptionHandler(notFoundException, webRequest)
+
+        assertErrorResponse(responseEntity, HttpStatus.NOT_FOUND, ControllerExceptionHandler.TYPE_NOT_FOUND_EXCEPTION, "'Entity' not found: '123'")
+    }
+
+    @Test
+    fun testAlreadyExistsExceptionHandler() {
+        val alreadyExistsException = AlreadyExistsException("Entity", "123")
+
+        val responseEntity: ResponseEntity<ControllerExceptionHandler.Error> =
+            controllerExceptionHandler.alreadyExistsExceptionHandler(alreadyExistsException, webRequest)
+
+        assertErrorResponse(responseEntity, HttpStatus.CONFLICT, ControllerExceptionHandler.TYPE_ALREADY_EXISTS_EXCEPTION, "'Entity' already exists: '123'")
+    }
+
+    @Test
+    fun testInvalidDomainStateExceptionHandler() {
+        val invalidDomainStateException = InvalidDomainStateException("Entity", "Invalid state")
+
+        val responseEntity: ResponseEntity<ControllerExceptionHandler.Error> =
+            controllerExceptionHandler.invalidDomainStateExceptionHandler(invalidDomainStateException, webRequest)
+
+        assertErrorResponse(responseEntity, HttpStatus.INTERNAL_SERVER_ERROR, ControllerExceptionHandler.TYPE_INVALID_DOMAIN_STATE_EXCEPTION, "found 'Entity' in invalid state: 'Invalid state'")
+    }
+
+    private fun assertErrorResponse(
+        responseEntity: ResponseEntity<ControllerExceptionHandler.Error>,
+        expectedStatus: HttpStatus,
+        expectedErrorType: String,
+        expectedErrorMessage: String,
+    ) {
+        assertEquals(expectedStatus, responseEntity.statusCode)
+        assertEquals(expectedErrorType, responseEntity.body?.error)
+    }
+}
