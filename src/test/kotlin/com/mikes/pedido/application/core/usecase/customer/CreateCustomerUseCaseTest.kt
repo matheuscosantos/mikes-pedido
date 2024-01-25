b68b973df96d7a4db63d7b2e336d79feedfca5b8
GAPrado <guilhermeprado@outlook.com>
2024-01-24 22:50:01 -0300

    add maps tests

---
+++ src/test/kotlin/com/mikes/pedido/application/core/usecase/customer/CreateCustomerUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.mikes.pedido.application.core.usecase.customer
+
+import com.mikes.pedido.application.core.domain.customer.Customer
+import com.mikes.pedido.application.core.domain.customer.valueobject.Cpf
+import com.mikes.pedido.application.mapper.customer.CustomerDomainMapper
+import com.mikes.pedido.application.port.outbound.customer.CustomerRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.Result.Companion.success
+
+internal class CreateCustomerUseCaseTest {
+    @Test
+    fun a() {
+        val cpf = "92979654078"
+        val exists = false
+        val expectedCustomer = mockCustomer(cpf)
+
+        val customerRepository = mockCustomerRepository(exists)
+        val customerDomainMapper = mockCustomerDomainMapper(expectedCustomer, cpf)
+
+        val createCustomerUseCase = CreateCustomerUseCase(customerRepository, customerDomainMapper)
+
+        val customer =
+            createCustomerUseCase.create(mockk())
+                .getOrThrow()
+
+        Assertions.assertEquals(expectedCustomer, customer)
+    }
+
+    private fun mockCustomerRepository(exists: Boolean) =
+        mockk<CustomerRepository>().also {
+            every { it.exists(any()) } returns exists
+            every { it.save(any()) } returns mockk()
+        }
+
+    private fun mockCustomerDomainMapper(
+        customer: Customer,
+        cpf: String,
+    ) = mockk<CustomerDomainMapper>().also {
+        every { it.new(any()) } returns success(customer)
+        every { it.new(any(), any(), any(), any()) } returns success(mockCustomer(cpf))
+    }
+
+    private fun mockCustomer(cpfValue: String) =
+        mockk<Customer>().also {
+            every { it.cpf } returns Cpf.new(cpfValue).getOrThrow()
+        }
+}
---
+++ src/test/kotlin/com/mikes/pedido/application/core/usecase/customer/FindCustomerUseCaseTest.kt
@@ -0,0 +1,52 @@
+package com.mikes.pedido.application.core.usecase.customer
+
+import com.mikes.pedido.application.core.domain.customer.Customer
+import com.mikes.pedido.application.core.domain.customer.valueobject.Cpf
+import com.mikes.pedido.application.mapper.customer.CustomerDomainMapper
+import com.mikes.pedido.application.port.outbound.customer.CustomerRepository
+import com.mikes.pedido.application.port.outbound.customer.dto.CustomerOutboundResponse
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.Result.Companion.success
+
+internal class FindCustomerUseCaseTest {
+    @Test
+    fun `when customer has found, expect success`() {
+        val cpf = "92979654078"
+        val active = true
+
+        val customerOutboundResponse = mockk<CustomerOutboundResponse>()
+        val expectedCustomer = mockk<Customer>()
+
+        val customerRepository = mockCustomerRepository(customerOutboundResponse)
+        val customerDomainMapper = mockCustomerDomainMapper(success(expectedCustomer))
+
+        val findCustomerService =
+            FindCustomerUseCase(
+                customerRepository,
+                customerDomainMapper,
+            )
+
+        val actualCustomer =
+            findCustomerService.find(cpf, active)
+                .getOrThrow()
+
+        verify { customerRepository.find(any(), eq(active)) }
+        verify { customerDomainMapper.new(eq(customerOutboundResponse)) }
+
+        Assertions.assertEquals(expectedCustomer, actualCustomer)
+    }
+
+    private fun mockCustomerRepository(nullableCustomerOutboundResponse: CustomerOutboundResponse?) =
+        mockk<CustomerRepository>().also {
+            every { it.find(any<Cpf>(), any()) } returns nullableCustomerOutboundResponse
+        }
+
+    private fun mockCustomerDomainMapper(customerResult: Result<Customer>) =
+        mockk<CustomerDomainMapper>().also {
+            every { it.new(any()) } returns customerResult
+        }
+}
